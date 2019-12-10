617abba469a08384aa72b1975c4754151b964856
eastarj <eastarj@gmail.com>
2019-12-11 04:29:12 +0900

    no message

--- eastarsharedpreferences/src/androidTest/java/dev/eastar/sharedpreferences/SharePreferencesHelperTest.kt
+++ eastarsharedpreferences/src/androidTest/java/dev/eastar/sharedpreferences/SharePreferencesHelperTest.kt
@@ -2,12 +2,20 @@ package dev.eastar.sharedpreferences
 
 import android.content.Context
 import androidx.core.content.edit
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import java.security.SecureRandom
 
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
 class SharePreferencesHelperTest {
     companion object {
         @Before
