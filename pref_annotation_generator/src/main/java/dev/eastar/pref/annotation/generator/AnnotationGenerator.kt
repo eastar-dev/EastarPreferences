4b7359a463f21062fb7a683af55eea6c05b65144
pluto <pluto@eastandroid.com>
2019-12-12 18:03:21 +0900

    no message

--- pref_annotation_generator/src/main/java/dev/eastar/pref/annotation/generator/AnnotationGenerator.kt
+++ pref_annotation_generator/src/main/java/dev/eastar/pref/annotation/generator/AnnotationGenerator.kt
@@ -17,11 +17,16 @@ package dev.eastar.pref.annotation.generator
 
 import dev.eastar.pref.annotation.Pref
 import dev.eastar.pref.annotation.util.Log
+import org.w3c.dom.Document
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 import java.io.File
+import java.util.*
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
+import javax.xml.parsers.DocumentBuilderFactory
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8) // to support Java 8
 @SupportedOptions(AnnotationGenerator.KAPT_KOTLIN_GENERATED_OPTION_NAME)
@@ -36,6 +41,8 @@ public class AnnotationGenerator : AbstractProcessor() {
         super.init(processingEnvironment)
         Log.processingEnvironment = processingEnv
         kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]!!
+        processingEnv.options
+
 
         generateInitializerClass(emptySet())
     }
@@ -77,7 +84,6 @@ public class AnnotationGenerator : AbstractProcessor() {
     private fun manifest() {
         val manifests = runCatching {
             Log.w("AndroidManifest.xml >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-            val kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]
             var dir = File(kaptKotlinGeneratedDir)
             while (dir.name != "build") dir = dir.parentFile
             Log.w(dir.toString())
@@ -89,14 +95,75 @@ public class AnnotationGenerator : AbstractProcessor() {
             Log.w(it.message ?: it.javaClass.name)
             it.stackTrace.forEach { Log.w(it.toString()) }
         }.getOrNull()
+
         manifests?.forEach {
             Log.w("$it")
+            parse(it)
 
             it.appendText("\n<!-- hello -->")
         }
+
     }
 
+    private fun parse(androidManifestFile: File) {
+        val doc: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(androidManifestFile)
+        val documentElement = doc.documentElement
+        documentElement.normalize()
+        val applicationPackage = documentElement.getAttribute("package")
+        var minSdkVersion = -1
+        var maxSdkVersion = -1
+        var targetSdkVersion = -1
+        val sdkNodes = documentElement.getElementsByTagName("uses-sdk")
+        if (sdkNodes.length > 0) {
+            val sdkNode = sdkNodes.item(0)
+            minSdkVersion = extractAttributeIntValue(sdkNode, "android:minSdkVersion", -1)
+            maxSdkVersion = extractAttributeIntValue(sdkNode, "android:maxSdkVersion", -1)
+            targetSdkVersion = extractAttributeIntValue(sdkNode, "android:targetSdkVersion", -1)
+        }
 
+        val applicationNodes = documentElement.getElementsByTagName("application")
+        var applicationClassQualifiedName: String? = null
+        var applicationDebuggableMode = false
+        if (applicationNodes.length > 0) {
+            val applicationNode = applicationNodes.item(0)
+            val nameAttribute = applicationNode.attributes.getNamedItem("android:name")
+            val debuggableAttribute = applicationNode.attributes.getNamedItem("android:debuggable")
+        }
+        val activityNodes = documentElement.getElementsByTagName("activity")
+
+        val serviceNodes = documentElement.getElementsByTagName("service")
+
+        val receiverNodes = documentElement.getElementsByTagName("receiver")
+
+        val providerNodes = documentElement.getElementsByTagName("provider")
+
+        val metaDataNodes = documentElement.getElementsByTagName("meta-data")
+        val usesPermissionNodes = documentElement.getElementsByTagName("uses-permission")
+        val permissionQualifiedNames: MutableList<String> = ArrayList()
+    }
+
+    private fun extractAttributeIntValue(node: Node, attribute: String, defaultValue: Int): Int {
+        try {
+            val attributes = node.attributes
+            if (attributes.length > 0) {
+                val attributeNode = attributes.getNamedItem(attribute)
+                if (attributeNode != null) {
+                    return attributeNode.nodeValue.toInt()
+                }
+            }
+        } catch (ignored: NumberFormatException) { // we assume the manifest is well-formed
+        }
+        return defaultValue
+    }
+
+    private fun extractComponentNames(applicationPackage: String, componentNodes: NodeList): List<String>? {
+        val componentQualifiedNames: MutableList<String> = ArrayList()
+        for (i in 0 until componentNodes.length) {
+            val activityNode = componentNodes.item(i)
+            val nameAttribute = activityNode.attributes.getNamedItem("android:name")
+        }
+        return componentQualifiedNames
+    }
 
 
     private fun generateInitializerClass(roundEnvironment: Set<Element>) {
@@ -115,6 +182,7 @@ public class AnnotationGenerator : AbstractProcessor() {
         //Log.w(ClassBuilderInitializer.androidManifest)
         //fileAndroidManifest.writeText(ClassBuilderInitializer.androidManifest)
     }
+
     private fun generatePrefClass(roundEnvironment: Element) {
         Log.w(roundEnvironment.toString())
 
