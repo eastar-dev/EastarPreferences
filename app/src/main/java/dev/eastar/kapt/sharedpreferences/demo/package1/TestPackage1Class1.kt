78f516e27b5aa96da24758ccf2123948519f0721
eastarj <eastarj@gmail.com>
2020-01-08 03:15:00 +0900

    use sample add

--- app/src/main/java/dev/eastar/kapt/sharedpreferences/demo/package1/TestPackage1Class1.kt
+++ app/src/main/java/dev/eastar/kapt/sharedpreferences/demo/package1/TestPackage1Class1.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused", "UNUSED_VARIABLE")
+
 package dev.eastar.kapt.sharedpreferences.demo.package1
 
 import dev.eastar.pref.annotation.Pref
@@ -6,4 +8,11 @@ import dev.eastar.pref.annotation.Pref
 interface TestPackage1Class1 {
     var package1Class1Value1: Boolean
     var package1Class1Value2: Int
+}
+
+
+fun useSample(){
+    val package1Class1Value1 = PrefTestPackage1Class1.getPackage1Class1Value1(true)
+    PrefTestPackage1Class1.package1Class1Value1 = true
+    val package1Class1Value2 = PrefTestPackage1Class1.package1Class1Value1
 }
